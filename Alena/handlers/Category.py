98a20bb76e02b8ae08e73a7d1357b1f0f18ccfff
Сергей Карасев <karasev-s@internet.ru>
2024-12-07 13:35:41 +0300

    f1
    f1

---
+++ Alena/handlers/Category.py
@@ -0,0 +1,210 @@
+import tb
+from aiogram import types
+from aiogram.bot import bot
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from keyboards import *
+# from main import date,datetime
+
+# from main import *
+
+class UserState(StatesGroup):
+    incity_b = State() #город вылета
+    country_b = State() # Страна прилета
+    vzr_b = State() # Количество взрослых
+    deti_b = State()# количество детей
+    hotel_b = State()# категория отеля
+    pitanie_b = State()# каегория питания
+    nochi_b = State() # количество ночей
+    name_b = State() # имя путешественика
+    email_b = State() # email путешественика
+    # дата
+
+
+async def incity(message):
+    with open('files/media/city_of_departure.jpg', "rb") as img:
+        await message.answer_photo(img, '<b>Выберите город вылета или напишите свой</b>', parse_mode='HTML',reply_markup=incity_kb)
+        await UserState.incity_b.set()
+async def handle_city_input(message: types.Message, state):
+    incity = message.text  # Получаем текст, введенный пользователем
+    await state.update_data(ic=incity)
+
+    # text = open("files\Zayavka.txt", 'w', encoding='utf-8')
+    # text.write(f'{day} Заявка для подбора тура\n ')
+    # text.close()
+    text = open("files\Zayavka.txt", 'w', encoding='utf-8')
+    text.write(f'Город вылета {incity}\n')
+    text.close
+    print(f'Город вылета {incity}')
+    data = await state.get_data()
+    incity = data['ic']
+    with open('files/media/strana.jpg', "rb") as img:
+        await message.answer_photo(img, '<b>Выберите страну или напишите свою</b>', parse_mode='HTML', reply_markup=country_kb)
+        await UserState.country_b.set()
+        await message.answer(f'Вы ввели город: {incity}')
+        # await message.answer()
+
+
+async def country(call:types.CallbackQuery, state):
+    incity = call.data
+    await state.update_data(ac=incity)
+    # text = open("files\Zayavka.txt", 'w', encoding='utf-8')
+    # text.write(f'{day} Заявка для подбора тура\n ')
+    # text.close()
+    text = open("files\Zayavka.txt",'w',encoding='utf-8')
+    text.write(f'Город вылета {incity}\n')
+    text.close
+    print(f'Город вылета {incity}')
+    data = await state.get_data()
+    incity = data['ac']
+    with open('files/media/strana.jpg', "rb") as img:
+        await call.message.answer_photo(img,'<b>Выберите страну или напишите свою</b>', parse_mode='HTML',reply_markup=country_kb)
+        await UserState.country_b.set()
+        await call.answer(f'Город вылета {incity}')
+        await call.answer()
+
+
+async def handle_country_input(message: types.Message, state):
+    country = message.text  # Получаем текст, введенный пользователем
+    await state.update_data(uc=country)
+    text = open("files\Zayavka.txt", 'a', encoding='utf-8')
+    text.write(f'Страна для путешествия {country}\n')
+    text.close
+    print(f'Страна для путешествия {country}')
+    data = await state.get_data()
+    country = data['uc']
+    with open('files/media/vzr.jpg', "rb") as img:
+        await message.answer_photo(img, '<b>Выберите количество взрослых путешествеников</b>', parse_mode='HTML', reply_markup=vzr_kb)
+        await UserState.vzr_b.set()
+        await message.answer(f'Вы ввели Страну для путешествия: {country}')
+        # await message.answer()
+
+
+async def vzr(call:types.CallbackQuery, state):
+    country = call.data
+    await state.update_data(bc=country)
+    text = open("files\Zayavka.txt", 'a', encoding='utf-8')
+    text.write(f'Страна для путешествия {country}\n')
+    text.close
+    print(f'Страна для путешествия {country}')
+    data = await state.get_data()
+    country = data['bc']
+    with open('files/media/vzr.jpg', "rb") as img:
+        await call.message.answer_photo(img,'<b>Выберите количество взрослых путешествеников</b>', parse_mode='HTML',reply_markup=vzr_kb)
+        await UserState.vzr_b.set()
+        await call.answer(f'Стана для путешествия {country}')
+        await call.answer()
+async def deti(call:types.CallbackQuery, state):
+    vzr = call.data
+    await state.update_data(cc=vzr)
+    text = open("files\Zayavka.txt", 'a', encoding='utf-8')
+    text.write(f'Количество взрослых {vzr}\n')
+    text.close
+    print(f'Количество взрослых {vzr}')
+    data = await state.get_data()
+    vzr = data['cc']
+    with open('files/media/deti.jpg', "rb") as img:
+        await call.message.answer_photo(img, '<b>Выберите количество детей в поездке</b>',parse_mode='HTML',reply_markup=deti_kb)
+        await UserState.deti_b.set()
+        await call.answer(f'Количество взрослых {vzr}')
+        await call.answer()
+async def hotel(call:types.CallbackQuery, state):
+    deti = call.data
+    await state.update_data(dc=deti)
+    text = open("files\Zayavka.txt", 'a', encoding='utf-8')
+    text.write(f'Количество детей {deti}\n')
+    text.close
+    print(f'Количество детей {deti}')
+    data = await state.get_data()
+    deti = data['dc']
+    with open('files/media/hotel.jpg', "rb") as img:
+        await call.message.answer_photo(img, '<b>Выберите минимальную категорию отеля</b>',parse_mode='HTML', reply_markup=hotel_kb)
+        await UserState.hotel_b.set()
+        await call.answer(f'Количество детей {deti}')
+        await call.answer()
+async def pitanie(call:types.CallbackQuery, state):
+    hotel = call.data
+    await state.update_data(ec=hotel)
+    text = open("files\Zayavka.txt", 'a', encoding='utf-8')
+    text.write(f'Минимальная категория отеля {hotel}\n')
+    text.close
+    print(f'Минимальная категория отеля {hotel}')
+    data = await state.get_data()
+    hotel = data['ec']
+    with open('files/media/pitanie.jpg', "rb") as img:
+        await call.message.answer_photo(img, '<b>Выберите питание</b>',
+                                   parse_mode='HTML', reply_markup=pitanie_kb)
+        await UserState.pitanie_b.set()
+        await call.answer(f'Минимальная категория отеля {hotel}')
+        await call.answer()
+
+async def nochi(call:types.CallbackQuery, state):
+    pitanie = call.data
+    await state.update_data(cc=pitanie)
+    text = open("files\Zayavka.txt", 'a', encoding='utf-8')
+    text.write(f'Категория питания {pitanie}\n')
+    text.close
+    print(f'Категория питания {pitanie}')
+    data = await state.get_data()
+    pitanie= data['cc']
+    with open('files/media/nochi.jpg', "rb") as img:
+        await call.message.answer_photo(img, '<b>Выберите количество ночей</b>',
+                                   parse_mode='HTML', reply_markup=nochi_kb)
+        await UserState.nochi_b.set()
+        await call.answer(f'Категория питания {pitanie}')
+        await call.answer()
+async def nochi_v(call:types.CallbackQuery, state):
+    nochi = call.data
+    await state.update_data(dc=nochi)
+    text = open("files\Zayavka.txt", 'a', encoding='utf-8')
+    text.write(f'Количество ночей {nochi}\n')
+    text.close
+    print(f'Количество ночей {nochi}')
+    with open('files/media/name.jpg', "rb") as img:
+        await call.message.answer_photo(img,'<b>Введите контактные данные для обратной связи </b>', parse_mode='HTML')
+    data = await state.get_data()
+    nochi= data['dc']
+    await UserState.nochi_b.set()
+    await call.answer(f'Количество ночей {nochi}')
+    await call.answer()
+
+async def handle_name_input(message: types.Message, state):
+    name = message.text  # Получаем текст, введенный пользователем
+    await state.update_data(uc=name)
+    text = open("files\Zayavka.txt", 'a', encoding='utf-8')
+    text.write(f'Контактные данные путешественика {name}\n')
+    text.close
+    print(f'Контактные данные {name}')
+    data = await state.get_data()
+    name = data['uc']
+    with open('files/media/email.jpg', "rb") as img:
+        await message.answer_photo(img, '<b>Напишите email для связи</b>', parse_mode='HTML')
+        await UserState.name_b.set()
+        await message.answer(f'Контактные данные: {name}')
+
+async def handle_email_input(message: types.Message, state):
+    email = message.text  # Получаем текст, введенный пользователем
+    await state.update_data(xc=email)
+    text = open("files\Zayavka.txt", 'a', encoding='utf-8')
+    text.write(f'email путешественика {email}')
+    text.close
+    print(f'email путешественика {email}')
+    data = await state.get_data()
+    email = data['xc']
+    await UserState.email_b.set()
+    await message.answer(f'email путешественика: {email}')
+    list = []
+    with open("files\Zayavka.txt", 'r', encoding='utf-8') as file:
+        for line in file:
+            list.append(line.rstrip('\n'))
+            print(list)
+    await message.answer(list)
+    await message.answer('<b>Проверьте данные и если все верно- нажмите кнопку "Подать заявку", что-то не так - "Редактировать"</b>',
+                         parse_mode='HTML', reply_markup=zayavka_kb)
+
+    # await message.answer(f'email путешественика: {email}')
+
+async def zayavka(message):
+    print('Делаю заявку')
+    with open('files/Zayavka.txt', 'rb') as doc:
+        bot.send_document(chat_id=message.chat.id, document=doc)
+        await message.answer('Заявка отправлена, с вами скоро свяжусь. Алена')
---
+++ Alena/main.py
@@ -0,0 +1,130 @@
+import aiogram
+from aiogram import Bot, Dispatcher, executor, types
+import asyncio
+import logging
+from aiogram.dispatcher.filters import Command, Text
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.types import InputMediaPhoto
+import os
+import config
+import database
+import datetime
+from datetime import date
+from datetime import datetime
+# from test import keyboard_callback
+
+api = config.API
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+class UserState(StatesGroup):
+    incity_b = State()
+    country_b = State()
+    vzr_b = State()
+    deti_b = State()
+    hotel_b = State()
+    pitanie_b = State()
+    nochi_b = State()
+    name_b = State()
+    email_b = State()
+
+import handlers.Start
+import handlers.Category
+import handlers.Admin
+d = date.today()
+day = d.strftime("%d/%m/%Y")
+current_time = datetime.now().time()
+
+text = open("files\Zayavka.txt",'w',encoding='utf-8')
+text.write(f'{day} Заявка для подбора тура\n ')
+text.close()
+dp.message_handler(lambda m: database.check_block(m.from_user.id))(handlers.Start.ban_message)
+dp.callback_query_handler(lambda c: database.check_block(c.from_user.id))(handlers.Start.ban_callbackquery)
+
+dp.message_handler(commands=['start'])(handlers.Start.start) # кнопка старт
+
+dp.message_handler(Text(equals=['ℹ️ О нас']))(handlers.Start.about_as)# кнопка о нас
+
+dp.message_handler(Text(equals=['✈️Поехали']))(handlers.Category.incity) # кнопка поехали
+
+dp.message_handler(state= UserState.incity_b)(handlers.Category.handle_city_input) # ввод incity
+
+
+dp.message_handler(Text(equals=['Подать заявку']))(handlers.Category.zayavka) # КНОПКА подать заявку
+
+# Обработчик Кнопок ввода города вылета
+dp.callback_query_handler(Text(equals='Москва'),state= UserState.incity_b)(handlers.Category.country)
+dp.callback_query_handler(Text(equals='Санкт Петербург'),state= UserState.incity_b)(handlers.Category.country)
+dp.callback_query_handler(Text(equals='Нижний Новгород'),state= UserState.incity_b)(handlers.Category.country)
+dp.callback_query_handler(Text(equals='Екатеринбург'),state= UserState.incity_b)(handlers.Category.country)
+
+dp.message_handler(state= UserState.country_b)(handlers.Category.handle_country_input) # ввод страны
+
+# Обработчик Кнопок ввода страны путешествия
+dp.callback_query_handler(Text(equals='Турция'),state= UserState.country_b)(handlers.Category.vzr)
+dp.callback_query_handler(Text(equals='Египет'),state= UserState.country_b)(handlers.Category.vzr)
+dp.callback_query_handler(Text(equals='Тайланд'),state= UserState.country_b)(handlers.Category.vzr)
+dp.callback_query_handler(Text(equals='ОАЭ'),state= UserState.country_b)(handlers.Category.vzr)
+dp.callback_query_handler(Text(equals='Россия'),state= UserState.country_b)(handlers.Category.vzr)
+dp.callback_query_handler(Text(equals='Китай'),state= UserState.country_b)(handlers.Category.vzr)
+dp.callback_query_handler(Text(equals='Абхазия'),state= UserState.country_b)(handlers.Category.vzr)
+dp.callback_query_handler(Text(equals='Вьетнам'),state= UserState.country_b)(handlers.Category.vzr)
+dp.callback_query_handler(Text(equals='Шри-Ланка'),state= UserState.country_b)(handlers.Category.vzr)
+dp.callback_query_handler(Text(equals='Мальдивы'),state= UserState.country_b)(handlers.Category.vzr)
+#
+# Обработчик Кнопок количества взрослых людей
+dp.callback_query_handler(Text(equals='🧍1 человек'),state= UserState.vzr_b)(handlers.Category.deti)
+dp.callback_query_handler(Text(equals='🧍🧍 2 человека'),state= UserState.vzr_b)(handlers.Category.deti)
+dp.callback_query_handler(Text(equals='3 человека'),state= UserState.vzr_b)(handlers.Category.deti)
+dp.callback_query_handler(Text(equals='4 человека'),state= UserState.vzr_b)(handlers.Category.deti)
+dp.callback_query_handler(Text(equals='5 человек'),state= UserState.vzr_b)(handlers.Category.deti)
+dp.callback_query_handler(Text(equals='6 человек'),state= UserState.vzr_b)(handlers.Category.deti)
+
+# Обработчик Кнопок количества детей
+dp.callback_query_handler(Text(equals='Без детей'),state= UserState.deti_b)(handlers.Category.hotel)
+dp.callback_query_handler(Text(equals='1 человечек'),state= UserState.deti_b)(handlers.Category.hotel)
+dp.callback_query_handler(Text(equals='2 человечка'),state= UserState.deti_b)(handlers.Category.hotel)
+dp.callback_query_handler(Text(equals='3 человечка'),state= UserState.deti_b)(handlers.Category.hotel)
+dp.callback_query_handler(Text(equals='4 человечка'),state= UserState.deti_b)(handlers.Category.hotel)
+dp.callback_query_handler(Text(equals='5 человечков'),state= UserState.deti_b)(handlers.Category.hotel)
+dp.callback_query_handler(Text(equals='6 человечков'),state= UserState.deti_b)(handlers.Category.hotel)
+
+# Обработчик Кнопок минимальной категории отеля
+dp.callback_query_handler(Text(equals='*'),state= UserState.hotel_b)(handlers.Category.pitanie)
+dp.callback_query_handler(Text(equals='**'),state= UserState.hotel_b)(handlers.Category.pitanie)
+dp.callback_query_handler(Text(equals='***'),state= UserState.hotel_b)(handlers.Category.pitanie)
+dp.callback_query_handler(Text(equals='****'),state= UserState.hotel_b)(handlers.Category.pitanie)
+dp.callback_query_handler(Text(equals='*****'),state= UserState.hotel_b)(handlers.Category.pitanie)
+
+
+# Обработчик Кнопок категории питания в отеле
+dp.callback_query_handler(Text(equals='любое'),state= UserState.pitanie_b)(handlers.Category.nochi)
+dp.callback_query_handler(Text(equals='все включено'),state= UserState.pitanie_b)(handlers.Category.nochi)
+dp.callback_query_handler(Text(equals='завтрак'),state= UserState.pitanie_b)(handlers.Category.nochi)
+dp.callback_query_handler(Text(equals='завтрак, ужин'),state= UserState.pitanie_b)(handlers.Category.nochi)
+dp.callback_query_handler(Text(equals='завтрак, обед, ужин'),state= UserState.pitanie_b)(handlers.Category.nochi)
+dp.callback_query_handler(Text(equals='ультра все включено'),state= UserState.pitanie_b)(handlers.Category.nochi)
+
+# Обработчик Кнопок количества ночей в отеле
+dp.callback_query_handler(Text(equals='6-8'),state= UserState.nochi_b)(handlers.Category.nochi_v)
+dp.callback_query_handler(Text(equals='9-11'),state= UserState.nochi_b)(handlers.Category.nochi_v)
+dp.callback_query_handler(Text(equals='12-14'),state= UserState.nochi_b)(handlers.Category.nochi_v)
+
+#обработчик ввода с клавиатуры имени путешественика
+dp.message_handler(state= UserState.nochi_b)(handlers.Category.handle_name_input)
+
+#обработчик ввода с клавиатуры email путешественика
+dp.message_handler(state= UserState.name_b)(handlers.Category.handle_email_input)
+
+# Обработчик Кнопок
+dp.message_handler(Text(equals=['Подать заявку']),state=UserState.email_b)(handlers.Category.zayavka)
+
+dp.message_handler(Text(equals=['Редактировать']),state=UserState.email_b)(handlers.Category.incity)
+
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates = True)
